cbdf133cb0ac236db00cb4d298d7e07ac4c42215
educ8tive-studio <educ8tive-studio@outlook.com>
2026-04-21 15:10:34 +1000

    Update gtag.js to use new Google Tag Manager URL
    Signed-off-by: educ8tive-studio <educ8tive-studio@outlook.com>

--- gtag.js
+++ gtag.js
@@ -2,7 +2,7 @@
 const script = document.createElement('script');
 script.async = true;
 // Use your primary ID in the URL
-script.src = "https://googletagmanager.com";
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-K57632CCGB";
 document.head.appendChild(script);
 
 // 2. Initialize gtag
@@ -14,3 +14,9 @@ gtag('js', new Date());
 gtag('config', 'G-K57632CCGB');      // Your GA4 ID
 gtag('config', 'GT-P3NSS9SX');      // Your Google Ads ID
 gtag('config', 'AW-17952891258');      // Another GA4 property
+
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;document.head.appendChild(j);
+})(window,document,'script','dataLayer','GTM-WJQVFQZD');
